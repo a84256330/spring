f08993dd47fab30e58e552208d7d8c7c9c19cf7d
ErvinMa <516889893@qq.com>
2019-12-18 22:13:30 +0800

    双指针

--- src/main/com.spring/leetCode/t2.java
+++ src/main/com.spring/leetCode/t2.java
@@ -9,10 +9,12 @@ public class t2 {
         int a = m-1;
         int b = n-1;
         int c = m+n-1;
-        while (a>0&&b>0){
+
+        while (a>=0 && b>=0){
             nums1[c--] = nums1[a]>nums2[b]?nums1[a--]:nums2[b--];
-            System.arraycopy(nums2,0,nums1,0,c+1);
         }
+        System.arraycopy(nums2,0,nums1,0,b+1);
+
     }
 
     public static void main(String[] args) {
@@ -20,6 +22,6 @@ public class t2 {
         int[] a = {1,2,3,0,0,0};
 
         int[] b = {4,5,6};
-
+        (new t2()).merge(a,3,b,3);
     }
 }
