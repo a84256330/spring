0322566f0a1b083161c9559ba5c92b08b737251d
a84256330 <mc5588zx@126.com>
2019-12-18 17:39:44 +0800

    合并两个有序数组

---
+++ src/main/com.spring/leetCode/t2.java
@@ -0,0 +1,25 @@
+package leetCode;
+
+/**
+ * @author ma
+ * @version 1.0 {2019/12/18}
+ */
+public class t2 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int a = m-1;
+        int b = n-1;
+        int c = m+n-1;
+        while (a>0&&b>0){
+            nums1[c--] = nums1[a]>nums2[b]?nums1[a--]:nums2[b--];
+            System.arraycopy(nums2,0,nums1,0,c+1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3,0,0,0};
+
+        int[] b = {4,5,6};
+
+    }
+}
